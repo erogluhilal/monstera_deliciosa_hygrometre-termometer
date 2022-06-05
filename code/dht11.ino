6a36661fec68de1bd40f9d2aab16b46c513f471c
erogluhilal <99879383+erogluhilal@users.noreply.github.com>
2022-06-05 22:06:56 +0300

    Update dht11.ino

--- code/dht11.ino
+++ code/dht11.ino
@@ -1,1 +1,36 @@
 
+
+
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+#define dhtpin 2
+#define dhttype DHT11
+char in;
+int temp;
+int hum;
+
+DHT dht(dhtpin,dhttype);
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+  
+}
+
+void loop()
+{
+  if(Serial.available()> 0)
+  {
+    in =(byte)Serial.read()&~(0x20); 
+    if(in== 'T')
+    {
+      temp = dht.readTemperature();
+      Serial.println(temp);
+    }else if (in== 'H')
+    {
+      hum = dht.readHumidity();
+      Serial.println(hum);
+    }
+  }
+}
